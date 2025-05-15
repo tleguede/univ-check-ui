84c5eda564ba154969f3cb0370c2d9295f97d4f9
Florentin Tchangai <tchangaiflorentin6@gmail.com>
2025-05-15 14:49:16 +0000

    Refactor AddOrganizationDialog component by removing unused imports and simplifying error handling

--- src/app/board/organizations/components/add-organization.dialog.tsx
+++ src/app/board/organizations/components/add-organization.dialog.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useCreateOrganisationMutation } from "@/hooks/queries/use-organizations.query";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -40,7 +40,7 @@ export function AddOrganizationDialog({ isOpen, onOpenChange, onSuccess }: AddOr
         onOpenChange(false);
         onSuccess?.();
       },
-      onError: (error:any) => {
+      onError: (error) => {
         console.error("Erreur:", error);
         toast.error("Erreur lors de la création de l'Organization");
       },
