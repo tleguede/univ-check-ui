25f1516be67bf17b4e212f3c3cd45529cd16af6e
Florentin Tchangai <tchangaiflorentin6@gmail.com>
2025-06-13 15:05:53 +0000

    Add index file to export components, utilities, hooks, and types for better module organization

---
+++ src/components/index.ts
@@ -0,0 +1,25 @@
+"use client";
+
+// Component exports
+export { AgendaView } from "./agenda-view";
+export { CalendarDndProvider, useCalendarDnd } from "./calendar-dnd-context";
+export { DayView } from "./day-view";
+export { DraggableEvent } from "./draggable-event";
+export { DroppableCell } from "./droppable-cell";
+export { EventCalendar } from "./event-calendar";
+export { EventDialog } from "./event-dialog";
+export { EventItem } from "./event-item";
+export { EventsPopup } from "./events-popup";
+export { MonthView } from "./month-view";
+export { WeekView } from "./week-view";
+
+// Constants and utility exports
+export * from "./constants";
+export * from "./utils";
+
+// Hook exports
+export * from "./use-current-time-indicator";
+export * from "./use-event-visibility";
+
+// Type exports
+export type { CalendarEvent, CalendarView, EventColor } from "./types";
